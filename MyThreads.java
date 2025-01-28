5b5279bd2ba2a600c28c9b887414dcd317c84496
jackonshiundu <jackonshiundu2019@gmail.com>
2025-01-28 15:55:29 +0300

    race-condition

--- MyThreads.java
+++ MyThreads.java
@@ -1,4 +1,4 @@
-class A extends Thread{
+class A implements Runnable{
     //every thread must have this run method
     public void run(){
         for(int i=0;i<5;i++){
@@ -20,17 +20,44 @@ class B implements Runnable{
         }
     }
 }
+class  Counter {
+
+    int count;
+    public synchronized void increment(){
+        count++;
+    }
+
+}
 
 public class MyThreads {
     public static void main(String[] args) {
         A obj1=new A();
         Runnable obj2=new B();
-
+        Counter one=new Counter();
+        //in this case i am trying to explai the concept of race condition where we have more that obe runnable or thread depending on a resource the count vaiable in this content
+        //so basically the expected aoutput should print diffren values as teh count values because basically the could be accessing the sma e rource at the same time and you will find that this will result to it beign updated only once to solve this we add synchronized before the method name then there will be some synchronization.
+        Runnable obj3=()->{
+            for(int i=0;i<10;i++){
+                one.increment();
+            }
+        };
+        Runnable obj4=()->{
+            for(int i=0;i<10;i++){
+                one.increment();
+            }
+        };
         //Since we changed the B object to runnable we have to  do this to make sure the start() runs the threads.
         //runnable don't have methods.
-        Thread t1=new Thread(obj2);
+        Thread t1=new Thread(obj1);
+        Thread t2=new Thread(obj2);
+        Thread t3=new Thread(obj3);
+        Thread t4=new Thread(obj4);
         //how to start a thread
-        t1.start();
-        obj2.start();
+        //t1.start();
+        //t2.start();
+        t3.start();
+        t4.start();
+
+        System.out.println(one.count);
     }
 }
