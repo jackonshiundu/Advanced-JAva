bb89d99556b2b0ab42098ab1f97bebf9d8aa362d
jackonshiundu <jackonshiundu2019@gmail.com>
2025-01-31 19:29:55 +0300

    Stream-api-starter

---
+++ StreamAPI.java
@@ -0,0 +1,14 @@
+//The Stream API in Java is a feature that allows you to process sequences of elements (such as collections) in a functional style, enabling operations like filtering, mapping, and reducing in a concise and readable way.
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamAPI {
+    public static void main(String[] args) {
+        //We are creating a list using List Interface
+        List<Integer> nums= Arrays.asList(3,76,89,8,47,4);
+
+        //given such a list we have diffrent ways we vcan iterate that list we can use a for loop or enhanced for loop.
+        //another way we can iterate is by use of methods provided by Stream API to iterate and do diffrent types of manipulation to the array.
+    }
+}
